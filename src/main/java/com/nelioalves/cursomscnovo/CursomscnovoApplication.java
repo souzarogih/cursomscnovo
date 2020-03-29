0401af10ab6af8bbf04bae04405cd6cb484596e2
Higor Souza <higor.andrade@hotmail.com>
2020-03-29 15:59:06 -0300

    Projeto criado

---
+++ src/main/java/com/nelioalves/cursomscnovo/CursomscnovoApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomscnovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomscnovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomscnovoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/cursomscnovo/CursomscnovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomscnovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomscnovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
