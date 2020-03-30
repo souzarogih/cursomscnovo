b6b26719dba1d1fd40d22a2c402f5534e1af0b8d
Higor Souza <higor.andrade@hotmail.com>
2020-03-30 20:20:29 -0300

    Criando operação de instanciação

--- src/main/java/com/nelioalves/cursomscnovo/CursomscnovoApplication.java
+++ src/main/java/com/nelioalves/cursomscnovo/CursomscnovoApplication.java
@@ -1,13 +1,34 @@
 package com.nelioalves.cursomscnovo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomscnovoApplication {
+import com.nelioalves.cursomscnovo.domain.Categoria;
+import com.nelioalves.cursomscnovo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomscnovoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomscnovoApplication.class, args);
+	}
+
+	//Método criado para salvar as informações básicas no banco de dados.
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
