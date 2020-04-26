c55bfbc4b43f095a5471eb8fc0d03611fade42af
Higor Souza <higor.andrade@hotmail.com>
2020-04-25 21:48:39 -0300

    Email HTML

--- src/main/java/com/nelioalves/cursomscnovo/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomscnovo/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomscnovo.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.nelioalves.cursomscnovo.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfimationEmail(Pedido obj);
 	
 	void sendMail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
