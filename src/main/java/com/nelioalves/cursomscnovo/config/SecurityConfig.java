f7a559a21d89a4b5220432d85e98dd1c79936ba1
Higor Souza <higor.andrade@hotmail.com>
2020-05-02 20:38:05 -0300

    Implementando autorização

--- src/main/java/com/nelioalves/cursomscnovo/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomscnovo/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nelioalves.cursomscnovo.security.JWTAuthenticationFilter;
+import com.nelioalves.cursomscnovo.security.JWTAuthorizationFilter;
 import com.nelioalves.cursomscnovo.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
