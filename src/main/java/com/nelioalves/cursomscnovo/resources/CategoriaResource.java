c990e64ef9f5b60b93650e013e5d8ecc16806e28
Higor Souza <higor.andrade@hotmail.com>
2020-05-02 21:53:54 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/nelioalves/cursomscnovo/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomscnovo/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,7 @@ public class CategoriaResource {
 	}
 	
 	/*Método para insert de uma nova Categoria*/
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){
 		Categoria obj = service.fromDTO(objDto);
@@ -45,6 +47,7 @@ public class CategoriaResource {
 	}
 	
 	/*Método para alterar uma Categoria*/
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
 		Categoria obj = service.fromDTO(objDto);
@@ -54,6 +57,7 @@ public class CategoriaResource {
 	}
 	
 	/*Método para deletar uma Categoria*/
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
