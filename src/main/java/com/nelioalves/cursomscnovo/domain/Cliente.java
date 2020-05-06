42832183e471c0e737796611244ea8fe40b6be67
Higor Souza <higor.andrade@hotmail.com>
2020-05-05 23:22:28 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/nelioalves/cursomscnovo/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomscnovo/domain/Cliente.java
@@ -56,9 +56,7 @@ public class Cliente implements Serializable{
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-	
-	private String imageUrl;
-	
+		
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);		
 	}
@@ -178,18 +176,6 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-	
-	
-	
+	}	
 	
 }
